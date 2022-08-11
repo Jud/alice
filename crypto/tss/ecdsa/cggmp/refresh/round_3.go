2c2eb2b2f98024565f9f089d1758c05ffdbad0fd
Jud <Jud.Stephenson@gmail.com>
2022-08-10 19:21:11 -0500

    Export refreshed share and partial pub key

--- crypto/tss/ecdsa/cggmp/refresh/round_3.go
+++ crypto/tss/ecdsa/cggmp/refresh/round_3.go
@@ -34,7 +34,12 @@ type round3Data struct {
 
 type Result struct {
 	refreshShare     *big.Int
+	Share            *big.Int
 	sumpartialPubKey map[string]*pt.ECPoint
+	PartialPublicKey map[string]*pt.ECPoint
+	PederssenParas   map[string]*paillierzkproof.PederssenOpenParameter
+	PaillierPrivateP *big.Int
+	PaillierPrivateQ *big.Int
 }
 
 type round3Handler struct {
@@ -197,9 +202,13 @@ func (p *round3Handler) Finalize(logger log.Logger) (types.Handler, error) {
 	sumpartialPubKey := pt.ScalarBaseMult(curve, p.refreshShare)
 	partialPubKey := make(map[string]*pt.ECPoint)
 	var err error
-	for _, peer := range p.peers {
+	pederssenParas := make(map[string]*paillierzkproof.PederssenOpenParameter, len(p.peers)+1)
+	pederssenParas[p.peerManager.SelfID()] = p.ped.PedersenOpenParameter
+
+	for id, peer := range p.peers {
 		plaintextShareBig := peer.round3.plaintextShareBig
 		refreshShare = refreshShare.Add(refreshShare, plaintextShareBig)
+		pederssenParas[id] = peer.round2.pederssenPara
 		sumpartialPubKey, err = sumpartialPubKey.Add(pt.ScalarBaseMult(curve, plaintextShareBig))
 		if err != nil {
 			return nil, err
@@ -218,7 +227,12 @@ func (p *round3Handler) Finalize(logger log.Logger) (types.Handler, error) {
 	}
 	p.result = &Result{
 		refreshShare:     refreshShare,
+		Share:            refreshShare,
 		sumpartialPubKey: partialPubKey,
+		PartialPublicKey: partialPubKey,
+		PederssenParas:   pederssenParas,
+		PaillierPrivateP: p.ped.GetP(),
+		PaillierPrivateQ: p.ped.GetQ(),
 	}
 	return nil, nil
 }
